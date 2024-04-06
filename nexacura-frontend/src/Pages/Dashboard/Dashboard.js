a8257b2a935a0a3115e5133f997b0277e0c4d46e
Balint Kaszap-Nagy <kaszap.nagy.balint@gmail.com>
2024-04-06 15:56:30 +0100

    Deleted avatar

--- nexacura-frontend/src/Pages/Dashboard/Dashboard.js
+++ nexacura-frontend/src/Pages/Dashboard/Dashboard.js
@@ -1,14 +1,10 @@
 import React from "react";
 import Layout from "../../Components/Dashboard/Layout";
-import AvatarMain from "../../Components/Avatar/AvatarMain";
 
 function Dashboard() {
   return (
     <Layout>
-      <div className="flex mt-3 h-full">
-        {/* <TreatmentList /> */}
-        <AvatarMain />
-      </div>
+      <div className="flex mt-3 h-full"></div>
     </Layout>
   );
 }
